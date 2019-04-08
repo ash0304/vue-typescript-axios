860b9304d601c427de252eeb333f3e4cf746f343
Ash <ash@hongyue.com>
2019-04-08 15:25:59 +0800

    vue.config.js 設定 排除build問題

---
+++ src/views/Page.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="page">
+        This is Page！
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component({
+
+})
+
+export default class Page extends Vue {}
+</script>
+
